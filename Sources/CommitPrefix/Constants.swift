14987f78fe14cdd483932060cbca062559149226
Stephen Martinez <enharmonicnuance@gmail.com>
2019-12-07 16:36:48 -0800

    [CPBugFix-001] Added a constants file to better help with string management

---
+++ Sources/CommitPrefix/Constants.swift
@@ -0,0 +1,22 @@
+//
+//  Constants.swift
+//
+//
+//  Created by Stephen Martinez on 12/7/19.
+//
+
+import Foundation
+
+public struct FileName {
+    
+    public static let commitPrefix = "CommitPrefix.txt"
+    public static let commitMessage = "commit-msg"
+    
+}
+
+public struct FolderName {
+    
+    public static let git = ".git"
+    public static let hooks = "hooks"
+    
+}
