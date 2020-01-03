67efb917b944608a98655cb11696ad03fa3252a1
Stephen Martinez <enharmonicnuance@gmail.com>
2020-01-02 19:42:37 -0800

    [CPFeature-008] Fixed Hook Script formatting issue with multi line string literals

--- Sources/CommitPrefix/CPInterfaceImpl.swift
+++ Sources/CommitPrefix/CPInterfaceImpl.swift
@@ -28,24 +28,29 @@ import Consler
 import Foundation
 import Files
 
-struct CPFileHandler {
+struct CommitPrefix: CPInterface {
     
-    private let cpInteractor: CPInteractor
+    private init() {}
     
-    init() throws {
+    private func getInteractor() throws -> CPInteractor {
         guard Folder.current.containsSubfolder(named: FolderName.git) else {
             throw CPError.notAGitRepo(currentLocation: Folder.current.path)
         }
         let gitDirectory = try Folder.current.subfolder(named: FolderName.git)
-        self.cpInteractor = try CPInteractor(gitDirectory: gitDirectory)
         try CommitMessageHook.findOrCreate(with: gitDirectory)
+        let cpInteractor = try CPInteractor(gitDirectory: gitDirectory)
+        return cpInteractor
     }
     
+    public static func interface() -> CPInterface { CommitPrefix() }
+    
     func outputPrefixes() throws -> ConslerOutput {
-        try cpInteractor.outputPrefixes()
+        let cpInteractor = try getInteractor()
+        return try cpInteractor.outputPrefixes()
     }
     
     func viewState() throws -> ConslerOutput {
+        let cpInteractor = try getInteractor()
         let cpState = try cpInteractor.getCommitPrefixState()
         switch cpState.mode {
         case .normal:
@@ -63,19 +68,23 @@ struct CPFileHandler {
     }
     
     func deletePrefixes() throws -> ConslerOutput {
-        try cpInteractor.deletePrefixes()
+        let cpInteractor = try getInteractor()
+        return try cpInteractor.deletePrefixes()
     }
     
     func writeNew(prefixes rawValue: String) throws -> ConslerOutput {
-        try cpInteractor.writeNew(prefixes: rawValue)
+        let cpInteractor = try getInteractor()
+        return try cpInteractor.writeNew(prefixes: rawValue)
     }
     
     func activateBranchMode(with validator: String) throws -> ConslerOutput {
-        try cpInteractor.activateBranchMode(with: validator)
+        let cpInteractor = try getInteractor()
+        return try cpInteractor.activateBranchMode(with: validator)
     }
     
     func activateNormalMode() throws -> ConslerOutput {
-        try cpInteractor.activateNormalMode()
+        let cpInteractor = try getInteractor()
+        return try cpInteractor.activateNormalMode()
     }
     
 }
