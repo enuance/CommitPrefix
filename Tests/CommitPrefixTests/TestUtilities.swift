2c0f7afbdfedc299c1a5804de2aefea88db146a6
Stephen Martinez <enharmonicnuance@gmail.com>
2020-01-20 11:38:00 -0800

    [CPFeature-010][Tests] Added test utilities file

--- Tests/CommitPrefixTests/TestUtilities.swift
+++ Tests/CommitPrefixTests/TestUtilities.swift
@@ -1,8 +1,48 @@
 //
-//  File.swift
-//  
+//  TestUtilities.swift
+//  commitPrefix
 //
-//  Created by Stephen Martinez on 1/20/20.
+//  MIT License
 //
+//  Copyright (c) 2019 STEPHEN L. MARTINEZ
+//
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in all
+//  copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+//  SOFTWARE.
 
 import Foundation
+import XCTest
+
+struct TestUtilities {
+    
+    static func urlOfExecutable(named executableName: String) -> URL {
+        productsDirectory.appendingPathComponent(executableName)
+    }
+    
+    /// Returns path to the built products directory.
+    private static var productsDirectory: URL {
+      #if os(macOS)
+        for bundle in Bundle.allBundles where bundle.bundlePath.hasSuffix(".xctest") {
+            return bundle.bundleURL.deletingLastPathComponent()
+        }
+        fatalError("Could not find the products directory")
+      #else
+        return Bundle.main.bundleURL
+      #endif
+    }
+    
+}
